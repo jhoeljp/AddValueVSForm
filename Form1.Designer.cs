f2c4756f59e65483fccbc68e8a511e1641f507f0
Jhoel Perez <jhoel.jjpr@gmail.com>
2018-09-15 21:54:09 -0500

    demo 1
    simple pop-up form for excel files

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AddValue
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.excelWrapper1 = new AddValue.UserControl1();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.webBrowser1);
+			this.panel1.Location = new System.Drawing.Point(202, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(586, 321);
+			this.panel1.TabIndex = 0;
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(586, 321);
+			this.webBrowser1.TabIndex = 0;
+			this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.button1);
+			this.panel2.Location = new System.Drawing.Point(12, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(184, 321);
+			this.panel2.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(42, 52);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "OpenFile";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "addvalue_excel";
+			//file format allowed on search
+			this.openFileDialog1.Filter = "Excel Files | *.xlsx";
+			// 
+			// excelWrapper1
+			// 
+			this.excelWrapper1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.excelWrapper1.Location = new System.Drawing.Point(0, 0);
+			this.excelWrapper1.Name = "excelWrapper1";
+			this.excelWrapper1.Size = new System.Drawing.Size(330, 250);
+			this.excelWrapper1.TabIndex = 0;
+			//this.excelWrapper1.ToolBarVisible = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "Add";
+			this.Text = "Form1 App";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private UserControl1 excelWrapper1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+	}
+}
+
---
+++ UserControl1.cs
@@ -0,0 +1,203 @@
+﻿//Auhtor: Jhoel Perez
+//Date: 6/25/2018
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using core = Microsoft.Office.Core;
+using excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+//add COM reference for microsoft office library and microsoft office excel
+
+
+namespace AddValue
+{
+	public partial class UserControl1 : UserControl
+	{
+		#region Contruction
+		public UserControl1()
+		{
+			InitializeComponent();
+		}
+		#endregion Construction
+
+		#region Fields
+		private string file_name = "mm_sheet.xlsx";
+		// Contains a reference to the hosting application
+		private excel.Application m_XlApplication = null;
+		// Contains a reference to the active workbook
+		private excel.Workbook m_Workbook = null;
+		private WebBrowser webBrowser1;
+
+		private readonly Missing MISS = Missing.Value;
+		private bool m_ToolBarVisible = false;
+		private Microsoft.Office.Core.CommandBar m_StandardCommandBar = null;
+		/// <summary>Contains the path to the workbook file.</summary>
+		private string m_ExcelFileName = string.Empty;
+		#endregion Fields
+
+		#region Events
+		private void OnWebBrowserExcelNavigated(object sender, WebBrowserNavigatedEventArgs e)
+		{
+			AttachApplication();
+		}
+
+		private void OnToolBarVisibleChanged()
+		{
+			try
+			{
+				m_StandardCommandBar.Visible = m_ToolBarVisible;
+			}
+			catch { }
+		}
+		#endregion Events
+
+		//#region Properties
+		//[Browsable(false)]
+		//public Workbook Workbook
+		//{
+		//	get { return m_Workbook; }
+		//}
+
+		//[Browsable(true), Category("Appearance")]
+		//public bool ToolBarVisible
+		//{
+		//	get { return m_ToolBarVisible; }
+		//	set
+		//	{
+		//		if (m_ToolBarVisible == value) return;
+		//		m_ToolBarVisible = value;
+		//		if (m_XlApplication != null) OnToolBarVisibleChanged();
+		//	}
+		//}
+		//#endregion Properties
+
+		#region Methods
+
+		public void file_open(string file)
+		{
+			if (!System.IO.File.Exists(file)) throw new Exception();
+			file_name = file;
+			//load work book in web browser
+			//find make webbrowser 
+			this.webBrowser1.Navigate(file, false);
+		}
+
+		public void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+		{
+			//if workbook of the same file_name doesnt exist
+			if ((m_Workbook = RetrieveWorkbook(file_name)) == null) return;
+			//excel
+			m_XlApplication = (Microsoft.Office.Interop.Excel.Application)m_Workbook.Application;
+		}
+
+		//pc executitnng programms list
+		[DllImport("ole32.dll")]
+		static extern int GetRunningObjectTable
+						(uint reserved, out IRunningObjectTable pprot);
+		[DllImport("ole32.dll")] static extern int CreateBindCtx(uint reserved, out IBindCtx pctx);
+		//fetch workbook
+		public excel.Workbook RetrieveWorkbook(string xlfile)
+		{
+			IRunningObjectTable prot = null;
+			IEnumMoniker pmonkenum = null;
+			try
+			{
+				IntPtr pfetched = IntPtr.Zero;
+				// Query the running object table (ROT)
+				if (GetRunningObjectTable(0, out prot) != 0 || prot == null) return null;
+				prot.EnumRunning(out pmonkenum); pmonkenum.Reset();
+				IMoniker[] monikers = new IMoniker[1];
+				while (pmonkenum.Next(1, monikers, pfetched) == 0)
+				{
+					IBindCtx pctx; string filepathname;
+					CreateBindCtx(0, out pctx);
+					// Get the name of the file
+					monikers[0].GetDisplayName(pctx, null, out filepathname);
+					// Clean up
+					Marshal.ReleaseComObject(pctx);
+					// Search for the workbook
+					if (filepathname.IndexOf(xlfile) != -1)
+					{
+						object roval;
+						// Get a handle on the workbook
+						prot.GetObject(monikers[0], out roval);
+						return roval as excel.Workbook;
+					}
+				}
+			}
+			catch
+			{
+				return null;
+			}
+			finally
+			{
+				// Clean up
+				if (prot != null) Marshal.ReleaseComObject(prot);
+				if (pmonkenum != null) Marshal.ReleaseComObject(pmonkenum);
+			}
+			return null;
+		}
+
+		protected void OnClosed(object sender, EventArgs e)
+		{
+			try
+			{
+				// Quit Excel and clean up.
+				if (m_Workbook != null)
+				{
+					m_Workbook.Close(true, Missing.Value, Missing.Value);
+					System.Runtime.InteropServices.Marshal.ReleaseComObject
+											(m_Workbook);
+					m_Workbook = null;
+				}
+				if (m_XlApplication != null)
+				{
+					m_XlApplication.Quit();
+					System.Runtime.InteropServices.Marshal.ReleaseComObject
+										(m_XlApplication);
+					m_XlApplication = null;
+					System.GC.Collect();
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Failed to close the application");
+			}
+		}
+
+		private void AttachApplication()
+		{
+			try
+			{
+				if (m_ExcelFileName == null || m_ExcelFileName.Length == 0) return;
+				// Creation of the workbook object
+				if ((m_Workbook = RetrieveWorkbook(m_ExcelFileName)) == null) return;
+				// Create the Excel.Application object
+				m_XlApplication = (Microsoft.Office.Interop.Excel.Application)m_Workbook.Application;
+				// Creation of the standard toolbar
+				m_StandardCommandBar = m_XlApplication.CommandBars["Standard"];
+				m_StandardCommandBar.Position = core.MsoBarPosition.msoBarTop;
+				m_StandardCommandBar.Visible = m_ToolBarVisible;
+				// Enable the OpenFile and New buttons
+				foreach (core.CommandBarControl control in m_StandardCommandBar.Controls)
+				{
+					string name = control.get_accName(Missing.Value);
+					if (name.Equals("Nouveau")) ((core.CommandBarButton)control).Enabled = false;
+					if (name.Equals("Ouvrir")) ((core.CommandBarButton)control).Enabled = false;
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Impossible de charger le fichier Excel");
+				return;
+			}
+		}
+		#endregion
+	}
+}
